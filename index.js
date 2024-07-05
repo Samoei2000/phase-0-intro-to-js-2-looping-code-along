f3af83e769563a531ca9a35adfde1380f6b4dff5
Your Name <princesslorna2000@gmail.com>
2024-07-05 16:28:29 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+
+    return messages;
+}
+
+function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+        console.log(i);
+    }
+}
